d7779067b73472c88c17afdfe9e2bdc5b0d12002
Dan <fleancu.daniel@gmail.com>
2018-04-18 20:38:06 +0300

    add bootstrap notify plugin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -88,7 +88,7 @@
       </a>
       <!-- END RESPONSIVE MENU TOGGLER -->
       <!-- BEGIN TOP NAVIGATION MENU -->
-      <div class="top-menu">
+      <div class="top-menu"> 
          <ul class="nav navbar-nav pull-right"> 
             <li class="dropdown dropdown-user">
                <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">

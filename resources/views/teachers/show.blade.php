4062018d382ad90e163edf1dad2bd24a1be57db5
Александр Черныш <johndoe@example.com>
2018-05-11 22:31:30 +0300

    Вёрстка страницы преподователя v1

--- resources/views/teachers/show.blade.php
+++ resources/views/teachers/show.blade.php
@@ -3,7 +3,64 @@
 @section('content')
 <div class="container no__home">
 	<div class="row">
-		Страница учителя
+		<div class="col-lg-3">
+			<img class="img-responsive img-circle" src="http://via.placeholder.com/400x400">
+			<div>
+			от 500 руб/час
+			</div>
+		</div>
+		<div class="col-lg-9">
+			<ul class="list-inline">
+				<li>Свободен</li>
+				<li>Добавить в избранное</li>
+			</ul>
+			<h1>Гапонова Маргарита Поликарповна</h1>
+			38 лет, опыт преподавания 5 лет <br>
+			ГОРОД:<br>
+			МОСКВА, ЦАО, ВОРОШИЛОВСКОЕ ШОССЕ<br>
+			СПЕЦИАЛИЗАЦИЯ: ПОДГОТОВКА К ЕГЭ ВСТУПИТЕЛЬНЫЕ ЭКЗАМЕНЫ 10-11 КЛАСС<br>
+			ПРЕДМЕТНАЯ ОБЛАСТЬ: МАТЕМАТИКА, ФИЗИКА ГЕОМЕТРИЯ<br>
+			ПОЛ:ЖЕНСКИЙ<br>
+			ОБРАЗОВАНИЕ <br>
+			ЮФУ (РГУ), биолого-почвенный факультет, специальность – биология, зоология, преподаватель химии и биологии<br>
+			О СЕБЕ<br>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.<br>
+			МЕСТА ПРОВЕДЕНИЯ ЗАНЯТИЙ<br>
+			<ul class="list-inline">
+				<li class="active">
+					<i class="fa fa-map-marker"></i>
+					МОСКВА, ЦАО, ВОРОШИЛОВСКОЕ ШОССЕ
+				</li>
+				<li>
+					<i class="fa fa-chevron-circle-down"></i>
+					ВОЗМОЖЕН ВЫЕЗД
+				</li>
+				<li>
+					<i class="fa fa-chevron-circle-down"></i>
+					ЗАНЯТИЯ В ГРУППЕ
+				</li>
+				<li>
+					<i class="fa fa-chevron-circle-down"></i>
+					ИНДИВИДУАЛЬНО
+				</li>
+				<li>
+					<i class="fa fa-chevron-circle-down"></i>
+					ОНЛАЙН
+				</li>
+			</ul>
+			ЛИЦЕНЗИИ И ДИПЛОМЫ
+			<div class="row">
+				<div class="col-lg-4">
+					<img class="img-responsive" src="http://via.placeholder.com/280x350">
+				</div>
+				<div class="col-lg-4">
+					<img class="img-responsive" src="http://via.placeholder.com/280x350">
+				</div>
+				<div class="col-lg-4">
+					<img class="img-responsive" src="http://via.placeholder.com/280x350">
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 @stop

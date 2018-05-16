5b08821421ef389cd254ee73879e3d8b28bc4cc8
Dan <fleancu.daniel@gmail.com>
2018-05-16 19:43:55 +0300

    test

--- resources/views/users/profile_types/teacher/edit.blade.php
+++ resources/views/users/profile_types/teacher/edit.blade.php
@@ -276,31 +276,30 @@
 			</div>
 			<div id="panel3" class="tab-pane fade">
                 
-                <div id="certificates__images" class="uploaderContainter" style="margin-bottom: 40px;">
-
-                	<div class="row">
-                		@foreach($user->certificates as $certificate)
-							<div class='col-md-4 load-thumbnail'> 
-	    		            	 
-	    		            	<div class="uploadedImg" 
-	    		            	     style="background-image: url(/public/uploads/users/certificates/{{ $certificate->image }})"></div>
-	    		            	<div class='actions__upload_img'>
-	    		            		<span onclick='deleteUploadImg(this, {{ $certificate->id }})' class="delete__upload_img"></span> 
-	                            </div>
-	        		     	</div>
-						@endforeach
-	                	 
-						<div class="col-md-4">
-							<div class="certificateLoadArea">
-								<input type="file" 
-							       name="diploms[]" 
-							       multiple 
-							       id="certificateInpuT" 
-							       onchange="multipleImages(this, '#certificates__images')">
-							     <span class="file__input_name"> Добавить или перетащить <br> сюда изображение</span>
-							</div>
+                <div id="certificates__images" class="row uploaderContainter" style="margin-bottom: 40px;">
+ 
+            		@foreach($user->certificates as $certificate)
+						<div class='col-md-4 load-thumbnail'> 
+    		            	 
+    		            	<div class="uploadedImg" 
+    		            	     style="background-image: url(/public/uploads/users/certificates/{{ $certificate->image }})"></div>
+    		            	<div class='actions__upload_img'>
+    		            		<span onclick='deleteUploadImg(this, {{ $certificate->id }})' class="delete__upload_img"></span> 
+                            </div>
+        		     	</div>
+					@endforeach
+                	 
+					<div class="col-md-4">
+						<div class="certificateLoadArea">
+							<input type="file" 
+						       name="diploms[]" 
+						       multiple 
+						       id="certificateInpuT" 
+						       onchange="multipleImages(this, '#certificates__images')">
+						     <span class="file__input_name"> Добавить или перетащить <br> сюда изображение</span>
 						</div>
-                	</div>
+					</div>
+                	 
 				</div>  
 					 
 			</div>

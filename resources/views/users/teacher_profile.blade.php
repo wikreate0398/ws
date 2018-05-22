2b94c9a1e8ff45ae937597e2f1d0fbbda18116f4
Александр Черныш <johndoe@example.com>
2018-05-22 19:59:35 +0300

    Изменения css

--- resources/views/users/teacher_profile.blade.php
+++ resources/views/users/teacher_profile.blade.php
@@ -42,7 +42,147 @@
 			</div>
 		</div>
 	</div> 
+	<!---->
+	<style>
+.cmn-toggle 
+{
+  position: absolute;
+  margin-left: -9999px;
+  visibility: hidden;
+}
 
+.cmn-toggle + label 
+{
+  display: block;
+  position: relative;
+  cursor: pointer;
+  outline: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+input.cmn-toggle-round-flat + label 
+{
+  padding: 2px;
+  width: 75px;
+  height: 30px;
+  background-color: #dddddd;
+  -webkit-border-radius: 60px;
+  -moz-border-radius: 60px;
+  -ms-border-radius: 60px;
+  -o-border-radius: 60px;
+  border-radius: 60px;
+  -webkit-transition: background 0.4s;
+  -moz-transition: background 0.4s;
+  -o-transition: background 0.4s;
+  transition: background 0.4s;
+  display: inline-block;
+}
+
+input.cmn-toggle-round-flat + label:before, input.cmn-toggle-round-flat + label:after 
+{
+  display: block;
+  position: absolute;
+  content: "";
+}
+
+input.cmn-toggle-round-flat + label:before 
+{
+  top: 2px;
+  left: 2px;
+  bottom: 2px;
+  right: 2px;
+  background-color: #fff;
+  -webkit-border-radius: 60px;
+  -moz-border-radius: 60px;
+  -ms-border-radius: 60px;
+  -o-border-radius: 60px;
+  border-radius: 60px;
+  -webkit-transition: background 0.4s;
+  -moz-transition: background 0.4s;
+  -o-transition: background 0.4s;
+  transition: background 0.4s;
+}
+
+input.cmn-toggle-round-flat + label:after 
+{
+  top: 4px;
+  left: 4px;
+  bottom: 4px;
+  width: 22px;
+  background-color: #dddddd;
+  -webkit-border-radius: 52px;
+  -moz-border-radius: 52px;
+  -ms-border-radius: 52px;
+  -o-border-radius: 52px;
+  border-radius: 52px;
+  -webkit-transition: margin 0.4s, background 0.4s;
+  -moz-transition: margin 0.4s, background 0.4s;
+  -o-transition: margin 0.4s, background 0.4s;
+  transition: margin 0.4s, background 0.4s;
+}
+
+input.cmn-toggle-round-flat:checked + label 
+{
+  background-color: #27A1CA;
+}
+
+input.cmn-toggle-round-flat:checked + label:after 
+{
+  margin-left: 45px;
+  background-color: #27A1CA;
+}
+
+.switch span{
+	display: inline-block;
+}
+	</style>
+	<div class="container teacher_lk">
+		<div class="row">
+			<div class="col-lg-3">
+				<img class="img-responsive" src="http://via.placeholder.com/400x400">
+			</div>
+			<div class="col-lg-9">
+				<span class="teacher_type">ПРЕПОДАВАТЕЛЬ</span>
+				<h1>Гапонова Маргарита Поликарповна</h1>
+				<div class="switch">
+					<span>Свободен</span>
+					<input id="cmn-toggle-4" class="cmn-toggle cmn-toggle-round-flat" type="checkbox">
+					<label for="cmn-toggle-4"></label>
+					<span>Занят</span>
+				</div>
+				<div>
+					<button class="btn btn-default">Публиковать профиль</button>
+					Для того, чтобы Вы появились в разделе репетиторов,  
+					вам нужно подробно заполнить свой профиль
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+                <ul class="nav nav-tabs registration_type" role="tablist">
+                    <li role="presentation" class="active">
+						<a href="#user" aria-controls="user" role="tab" data-toggle="tab">МОИ КУРСЫ (ОБУЧАЮ)</a>
+					</li>
+                    <li role="presentation">
+						<a href="#teacher" aria-controls="teacher" role="tab" data-toggle="tab">МОИ ЗАЯВКИ</a>
+                    </li>
+                    <li role="presentation">
+						<a href="#university" aria-controls="university" role="tab" data-toggle="tab">ПОДПИСКИ</a>
+					</li>
+                    <li role="presentation">
+						<a href="#university" aria-controls="university" role="tab" data-toggle="tab">УЧЕБНЫЕ ЗАВЕДЕНИЯ</a>
+					</li>
+                    <li role="presentation">
+						<a href="#university" aria-controls="university" role="tab" data-toggle="tab">ОТЗЫВЫ И КОММЕНТАРИИ</a>
+					</li>
+                </ul>
+			</div>
+		</div>
+	</div>
+	<!---->
 	<nav class="navbar navbar-default">
 	  <div class="container"> 
 	    <ul class="nav navbar-nav">

26791cceedf821b01b708d6b870ef9c3fe8a4a44
Александр Черныш <johndoe@example.com>
2018-08-13 22:49:17 +0300

    Ровные блоки в учебке

--- public/js/custom.js
+++ public/js/custom.js
@@ -538,9 +538,9 @@ function eqBlocksInit()
     setEqualHeight2($('.external_card h3'), $('.course__catalog'));
     setEqualHeight2($('.external_card h4'), $('.course__catalog'));
     setEqualHeight($('#teacher_carousel .item h3'));
+    setEqualHeight($('.external_univer h3'));
     setEqualHeight2($('.course_card .footer__course_card .set__going_date'), $('.course__catalog'));
     setEqualHeight2($('.university__item h3'), $('.universities_catalog'));
-    setEqualHeight2($('.external_univer h3'), $('.universities_catalog'));
 
     setEqualHeight2($('.item__block .item__name'), $('.teacher__universities'));
     setEqualHeight2($('.item__block .item__footer'), $('.teacher__universities')); 

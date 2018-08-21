0e0f92097e81b9c74c1b690fc74f78eddf9d7da0
Александр Черныш <johndoe@example.com>
2018-08-21 21:18:17 +0300

    Подделится

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -28,6 +28,10 @@
 						</div>
 						<div class="news__text">{!! $data['text'] !!}</div>
 						<br>
+						<script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
+						<script src="//yastatic.net/share2/share.js"></script>
+						<div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,twitter,viber,whatsapp,skype,telegram"></div>
+						<br>
 						<a href="/news" class="btn btn2">Назад к новостям</a>
 					</div>
 				</div>
